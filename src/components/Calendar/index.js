8f56c4126254a0bbd45f8c572f499a5762a728bb
arturarushanyan <arthur.arushanyan92@gmail.com>
2018-05-18 21:26:35 +0400

    Basic structure and libs added

---
+++ src/components/Calendar/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+
+export default class Calendar extends Component {
+    render() {
+        return (
+            <div className="calendar-container">
+                <h5>calendar</h5>
+            </div>
+        )
+    }
+}
